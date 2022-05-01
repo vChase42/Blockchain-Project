31f545e81f63702b913b5f922e6113171d137b97
vChase42 <chasten@ucsb.edu>
2022-04-30 21:28:50 -0700

    edited cloud.js slightly

--- src/cloudFunctions/cloud.js
+++ src/cloudFunctions/cloud.js
@@ -1,10 +1,10 @@
-import Moralis from "moralis/types";
 
 Moralis.Cloud.define("getTransactions",async(request) => {
     const logger = Moralis.Cloud.getLogger();
     const address = request.params.address.toLowerCase();
     const squery = new Moralis.Query("_AddressSyncStatus");
     squery.equalTo("address",address);
+    const queryCount = await squery.count();
 
     if(Number(queryCount) <= 0) {
         await Moralis.Cloud.run("watchEthAdress", {address: address, sync_historical: true}, {useMasterKey: true});
@@ -14,12 +14,13 @@ Moralis.Cloud.define("getTransactions",async(request) => {
     const query1 = new Moralis.Query("EthTransactions")
     query1.equalTo("from_address", address);
     
-    const query2 = new Moralis.Query("EithTransactions");
+    const query2 = new Moralis.Query("EthTransactions");
     query2.equalTo("to_address", address);
     
     const query = Moralis.Query.or(query1, query2);
     query.descending("block_number");
     query.withCount();
     const results = await query.find();
-    return (synced: true, results: results);
+    return {synced: true, results: results};
 });
+
